16e2ab787166c4a6222f21ad0d3828086a3f67df
Koshak1432 <70834575+Koshak1432@users.noreply.github.com>
2023-10-11 17:51:10 +0700

    create spring boot apll

---
+++ src/main/java/nsu/medpollback/MedpollBackApplication.java
@@ -0,0 +1,13 @@
+package nsu.medpollback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedpollBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedpollBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nsu/medpollback/MedpollBackApplicationTests.java
@@ -0,0 +1,13 @@
+package nsu.medpollback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedpollBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
