b1e3dad86afeecdff8bb8d33c2b49d0e7d11da19
Koshak1432 <70834575+Koshak1432@users.noreply.github.com>
2023-12-04 19:50:35 +0700

    implemented get prescription by it's id

--- src/main/java/nsu/medpollback/controllers/PrescriptionController.java
+++ src/main/java/nsu/medpollback/controllers/PrescriptionController.java
@@ -1,17 +1,20 @@
 package nsu.medpollback.controllers;
 
+import jakarta.validation.constraints.Positive;
 import nsu.medpollback.config.Constants;
 import nsu.medpollback.model.dto.PrescriptionDto;
 import nsu.medpollback.model.exceptions.AuthException;
 import nsu.medpollback.model.exceptions.BadRequestException;
+import nsu.medpollback.model.exceptions.NotFoundException;
 import nsu.medpollback.services.PrescriptionService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RequestMapping(Constants.BASE_API_PATH + "/prescriptions")
 @CrossOrigin(maxAge = 1440)
-@PreAuthorize("hasAuthority('DOCTOR')")
 @Validated
 @RestController
 public class PrescriptionController {
@@ -21,9 +24,16 @@ public class PrescriptionController {
         this.prescriptionService = prescriptionService;
     }
 
+    @PreAuthorize("hasAuthority('DOCTOR')")
     @PostMapping
     public Long addPrescription(@RequestBody PrescriptionDto prescriptionDto) throws BadRequestException,
             AuthException {
         return prescriptionService.addPrescription(prescriptionDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public PrescriptionDto getPrescriptionById(@PathVariable @Positive Long id, @RequestParam(required = false) UUID cardUUID) throws
+            NotFoundException {
+        return prescriptionService.getPrescriptionById(id, cardUUID);
     }
 }
--- src/main/java/nsu/medpollback/services/PrescriptionService.java
+++ src/main/java/nsu/medpollback/services/PrescriptionService.java
@@ -3,7 +3,11 @@ package nsu.medpollback.services;
 import nsu.medpollback.model.dto.PrescriptionDto;
 import nsu.medpollback.model.exceptions.AuthException;
 import nsu.medpollback.model.exceptions.BadRequestException;
+import nsu.medpollback.model.exceptions.NotFoundException;
+
+import java.util.UUID;
 
 public interface PrescriptionService {
+    PrescriptionDto getPrescriptionById(Long id, UUID cardUUID) throws NotFoundException;
     Long addPrescription(PrescriptionDto prescriptionDto) throws BadRequestException, AuthException;
 }
